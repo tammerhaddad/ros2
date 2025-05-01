06493d527e7879ad66482bfe7ccd1f3b05453344
Tammer Haddad <tammerrh@gmail.com>
2025-05-01 13:33:19 -0400

    added wvh_guide node controller

---
+++ wvh_guide_server/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'wvh_guide_server'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tammerhaddad',
+    maintainer_email='tammerhaddad@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'guide_server = wvh_guide_server.guide_server:main',
+        ],
+    },
+)
---
+++ wvh_guide_server/wvh_guide_server/guide_server.py
@@ -0,0 +1,46 @@
+import rclpy
+import time
+from rclpy.action import ActionServer
+from rclpy.node import Node
+
+from trh_msgs.action import StringAction
+from wvh_guide import run
+
+class GuideServer(Node):
+
+    def __init__(self):
+        super().__init__('guide_server')
+        self._action_server = ActionServer(
+            self,
+            StringAction,
+            'guide_server',
+            self.execute_callback)
+        self.get_logger().info('Init done.')
+
+    def execute_callback(self, goal_handle):
+        self.get_logger().info('Executing goal...')
+        result = StringAction.Result()
+        path = goal_handle.request.strrequest.split(",")
+        summary, path = run(
+            start=path[0],
+            goal=path[1],
+            show_map=False
+        )
+
+        print("\n".join(summary))
+        print("Path:", path)
+        result.strresult = str(summary)
+        goal_handle.succeed()
+        return result
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    action_server = GuideServer()
+
+    rclpy.spin(action_server)
+
+
+if __name__ == '__main__':
+    main()
